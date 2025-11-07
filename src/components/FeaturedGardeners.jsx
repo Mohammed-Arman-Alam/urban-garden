d1e1ff0d3a5e9bf5b175aeea1d79f3002e91f920
MaYa <armanalam2410@gmail.com>
2025-11-07 19:02:33 +0600

    updated

--- src/components/FeaturedGardeners.jsx
+++ src/components/FeaturedGardeners.jsx
@@ -13,11 +13,11 @@ const FeaturedGardeners = () => {
     return (
         <div className='w-10/12 mx-auto  my-10'>
             <h1 className='text-center font-bold text-3xl my-8 text-[#2E7D32]'>Featured Gardeners</h1>
-            <div className="flex justify-between">
+            <div className="grid grid-cols-2 sm:grid-cols-6">
                  {
-                activeGardeners.map(gardeners=><div>
+                activeGardeners.map(gardeners=><div className='mx-auto'>
                     <img src={gardeners.photoUrl} className='w-27 hover:w-30 rounded-full border-1 border-[#2e7d32]'></img>
-                    <p className='text-center mt-2 font-semibold text-lg'>{gardeners.name}</p>
+                    <p className='mt-2 font-semibold text-lg'>{gardeners.name}</p>
                 </div>)
             }
             </div>
